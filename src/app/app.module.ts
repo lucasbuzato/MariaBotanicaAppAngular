85839492d64580dfe26418b6bcbefa300cfc5a46
vinihiga21 <167332326+vinihiga21@users.noreply.github.com>
2024-09-11 16:24:31 -0300

    MarketPlace Quase Feito

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeScreenComponent } from './home-screen/home-screen.component';
 import { MarketPlaceScreenComponent } from './market-place-screen/market-place-screen.component';
+import { CardComponent } from './card/card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeScreenComponent,
-    MarketPlaceScreenComponent
+    MarketPlaceScreenComponent,
+    CardComponent
   ],
   imports: [
     BrowserModule,
